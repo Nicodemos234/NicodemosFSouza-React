ebb20696f3b5a97456e7407b648e9435214da38f
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-19 21:29:19 -0300

    Home text in english

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,25 +11,25 @@ import Twitter from '../../assets/images/social/twitter.png';
 import Youtube from '../../assets/images/social/youtube.png';
 
 const Home = () => (
-    
+
     <div className="home-content fill-avaliable center center-elements">
         <Helmet>
-          <title>Nicodemos</title>
+            <title>Nicodemos</title>
         </Helmet>
         <div className="content">
-            <h1>Seja bem-vindo</h1>
-            <p>Olá, meu nome é Nicodemos, tenho 18
-            anos, sou programador, editor de vídeos e
-            apaixonado por tecnologia.</p>
+            <h1>Welcome</h1>
+            <p>Hi, my name is Nicodemos, I have 18 years old,
+            I'm programmer, video editor and
+            passionate about programming.</p>
             <div>
-                <a target="_blank" href="https://www.twitch.tv/nicodemos234"><img src={Twitch} alt="Twitch icon"/></a>
-                <a target="_blank" href="https://youtube.com/nicodemos234"><img src={Youtube} alt="Youtube icon"/></a>
-                <a target="_blank" href="https://www.instagram.com/nicodemos234/"><img src={Instagram} alt="Instagram icon"/></a>
-                <a target="_blank" href="https://twitter.com/nicodemos234"><img src={Twitter} alt="Twitter icon"/></a>
+                <a target="_blank" href="https://www.twitch.tv/nicodemos234"><img src={Twitch} alt="Twitch icon" /></a>
+                <a target="_blank" href="https://youtube.com/nicodemos234"><img src={Youtube} alt="Youtube icon" /></a>
+                <a target="_blank" href="https://www.instagram.com/nicodemos234/"><img src={Instagram} alt="Instagram icon" /></a>
+                <a target="_blank" href="https://twitter.com/nicodemos234"><img src={Twitter} alt="Twitter icon" /></a>
             </div>
         </div>
         <div>
-            <img src={Nico} alt="Nico picture"/>
+            <img src={Nico} alt="Nico picture" />
         </div>
     </div>
 );
